4d9f54153f7323c8b5d7f715c6d6858ae033f680
David Taylor <dtaylor@evertz.com>
2022-12-11 16:12:51 +0000

    Cleanup

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,7 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-
-import { env } from "../../../env/server.mjs";
-import { createContext } from "../../../server/trpc/context";
 import { appRouter } from "../../../server/trpc/router/_app";
+import { createContext } from "../../../server/trpc/context";
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { env } from "../../../env/server.mjs";
 
 // export API handler
 export default createNextApiHandler({
