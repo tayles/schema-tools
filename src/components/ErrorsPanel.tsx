c731d3bc9759b693435c3430e40e3e01f22c1e85
David Taylor <dtaylor@evertz.com>
2022-12-17 01:57:48 +0000

    Error styling

--- src/components/ErrorsPanel.tsx
+++ src/components/ErrorsPanel.tsx
@@ -1,3 +1,5 @@
+import { Alert } from '@mantine/core';
+import { IconAlertCircle } from '@tabler/icons';
 import Panel from './Panel';
 import { useSchemaStore } from '@/store/state';
 
@@ -11,25 +13,14 @@ const ErrorsPanel = () => {
     <Panel title="Errors">
       <div className="flex flex-col items-stretch gap-2">
         {allErrors.map((error) => (
-          <div
+          <Alert
             key={error.message}
-            className="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
-            role="alert"
+            icon={<IconAlertCircle size={16} />}
+            title="Bummer!"
+            color="red"
           >
-            <strong className="font-bold">Schema</strong>
-            <span className="block sm:inline">{error.message}</span>
-            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-              <svg
-                className="h-6 w-6 fill-current text-red-500"
-                role="button"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <title>Close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-              </svg>
-            </span>
-          </div>
+            {error.message}
+          </Alert>
         ))}
       </div>
     </Panel>
