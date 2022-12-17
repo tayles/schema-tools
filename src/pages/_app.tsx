70eeb5a986ca859e9ae1307254d67dd5bfde8be2
David Taylor <dtaylor@evertz.com>
2022-12-17 00:59:35 +0000

    Add mantine UI component library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { type AppType } from 'next/app';
+import { MantineProvider } from '@mantine/core';
 
 import { trpc } from '@/utils/trpc';
-
 import '@/styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      theme={{
+        colorScheme: 'light',
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
